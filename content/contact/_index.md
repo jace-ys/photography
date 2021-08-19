acbb2b168a16d27da172845b1d8869331d31e21c
jace-ys <jaceys.tan@gmail.com>
2021-08-19 16:24:33 +0800

    Add social link to Twitter

--- content/contact/_index.md
+++ content/contact/_index.md
@@ -1,7 +1,5 @@
 +++
 title = "Contact"
-description = "Get in touch with me by leaving a message using the form below. You can also reach out via email or Instagram."
+description = "Get in touch with me by leaving a message using the form below. You can also reach out via email or social media."
 location = "London, United Kingdom"
-email = "jaceys.tan@gmail.com"
-instagram = "jaceys_"
 +++
--- themes/stardust/static/js/main.js
+++ themes/stardust/static/js/main.js
@@ -1,56 +1,56 @@
 "use strict";
 
 (function () {
-	var initMasonry = function () {
-		$(".masonry").each(function () {
-			var grid = $(this).find(".grid").masonry({
-				itemSelector: ".grid-item",
-				columnWidth: ".grid-sizer",
-				percentPosition: true,
-			});
+  var initMasonry = function () {
+    $(".masonry").each(function () {
+      var grid = $(this).find(".grid").masonry({
+        itemSelector: ".grid-item",
+        columnWidth: ".grid-sizer",
+        percentPosition: true,
+      });
 
-			grid.imagesLoaded().progress(function () {
-				grid.masonry("layout");
-			});
-		});
-	};
+      grid.imagesLoaded().progress(function () {
+        grid.masonry("layout");
+      });
+    });
+  };
 
-	var initTyped = function () {
-		$(".typed--content").each(function () {
-			var typed = new Typed(".typed", {
-				stringsElement: $(this).find(".typed--strings")[0],
-				typeSpeed: 30,
-				backSpeed: 15,
-				startDelay: 1500,
-				smartBackspace: true,
-				loop: true,
-			});
-		});
-	};
+  var initTyped = function () {
+    $(".typed--content").each(function () {
+      var typed = new Typed(".typed", {
+        stringsElement: $(this).find(".typed--strings")[0],
+        typeSpeed: 30,
+        backSpeed: 15,
+        startDelay: 1500,
+        smartBackspace: true,
+        loop: true,
+      });
+    });
+  };
 
-	var initPreloader = function () {
-		$(window).on("load", function () {
-			$("#loader").fadeOut("slow", function () {
-				$("#preloader").delay(300).fadeOut("slow");
-			});
-		});
-	};
+  var initPreloader = function () {
+    $(window).on("load", function () {
+      $("#loader").fadeOut("slow", function () {
+        $("#preloader").delay(300).fadeOut("slow");
+      });
+    });
+  };
 
-	var initToggleMenu = function () {
-		var toggle = $(".header-menu-toggle");
-		var nav = $(".header-nav-wrap");
+  var initToggleMenu = function () {
+    var toggle = $(".header-menu-toggle");
+    var nav = $(".header-nav-wrap");
 
-		toggle.on("click", function (event) {
-			event.preventDefault();
-			toggle.toggleClass("is-clicked");
-			nav.slideToggle();
-		});
-	};
+    toggle.on("click", function (event) {
+      event.preventDefault();
+      toggle.toggleClass("is-clicked");
+      nav.slideToggle();
+    });
+  };
 
-	(function init() {
-		initMasonry();
-		initTyped();
-		initPreloader();
-		initToggleMenu();
-	})();
+  (function init() {
+    initMasonry();
+    initTyped();
+    initPreloader();
+    initToggleMenu();
+  })();
 })();
